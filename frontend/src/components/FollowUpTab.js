3c9ad7361d65ac2c79e27e241981eac4956bd557
NandhiniRavi01 <113678413+NandhiniRavi01@users.noreply.github.com>
2026-01-03 15:12:56 +0530

    Update FollowUpTab.js

--- frontend/src/components/FollowUpTab.js
+++ frontend/src/components/FollowUpTab.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { FiClock, FiRefreshCw } from "react-icons/fi";
 import "./FollowUpTab.css";
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://65.1.129.37:5000";
 
 // Helper function to make authenticated requests
 const makeAuthenticatedRequest = async (url, options = {}) => {
@@ -86,3 +86,4 @@ function FollowUpTab() {
 }
 
 export default FollowUpTab;
+
